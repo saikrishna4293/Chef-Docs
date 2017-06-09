633845391624f0e56e45a36ba4c95510e0b45f0b
saikrishna4293 <krishna4charan@gmail.com>
2017-06-09 16:28:50 +0000

    updated on 06/09/17

--- cookbooks/file/recipes/default.rb
+++ cookbooks/file/recipes/default.rb
@@ -5,5 +5,10 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 file "/etc/workspace/file1.txt" do
+	#action :delete
 	content "This is Saikrishna"
+end
+
+cookbook_file "/etc/workspace/file2.txt"
+	source "file2.txt"
 end
